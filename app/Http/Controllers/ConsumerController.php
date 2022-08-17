d9413adaf80d58d2e76b408c8cefd4f91109bd85
anthonya <anthonya@cloudstaff.com>
2022-08-17 22:53:54 +0800

    a

--- app/Http/Controllers/ConsumerController.php
+++ app/Http/Controllers/ConsumerController.php
@@ -95,7 +95,7 @@ class ConsumerController extends Controller
             }
             if ($carouselRemainder > 0) {
                 $products = [];
-                for ($product = 0; $product < $carouselRemainder; $product++) {
+                for ($product = 1; $product <= $carouselRemainder; $product++) {
                     array_push($products, $inventory[$product*(((int)$carouselPage)+1)]);
                 }
                 $item = [
