eed517966ab7f862be0a0068a30de6585ec65957
anthonya <anthonya@cloudstaff.com>
2022-08-17 22:28:10 +0800

    a

--- app/Http/Controllers/ConsumerController.php
+++ app/Http/Controllers/ConsumerController.php
@@ -98,6 +98,12 @@ class ConsumerController extends Controller
                 for ($product = 0; $product < $carouselRemainder; $product++) {
                     array_push($products, $inventory[$product*$carouselPage+1]);
                 }
+                $item = [
+                    'page' => $carouselPage+1,
+                    'products' => $products
+                ];
+                array_push($data, $item);
+
             }
             $this->response_message['status'] = 'success';
             $this->response_message['message'] = 'Store successfully retrieved.';
